3ebf8f07c1727f7ee2499097fb0a4bff9701fb89
Bread <sherzodahmedov1007@gmail.com>
2026-03-05 17:28:45 +0500

    Add files via upload

---
+++ dataclass.py
@@ -0,0 +1,120 @@
+from dataclasses import dataclass
+
+# # 1- masala
+#
+# @dataclass
+# class Talaba:
+#     ism: str
+#     yosh: int
+#     kurs: int
+#
+# student1 = Talaba("Kamol", 19, 2)
+#
+# print(student1)
+
+# # 2 - masala
+#
+# @dataclass
+# class Kitob:
+#     nomi: str
+#     muallif: str
+#     sahifa_soni: int
+#
+# kitob1 = Kitob("Men", "Aziz", 300)
+#
+# print(kitob1)
+
+# # 3 - masala
+#
+# @dataclass
+# class Telefon:
+#     brand: str
+#     model: str
+#     narx: int
+#
+# Telefon1 = Telefon("iphone", "16e", 699)
+#
+# print(Telefon1)
+
+# # 4 - masala
+#
+# @dataclass
+# class Avtomobil:
+#     marka: str
+#     rang: str
+#     yil: int
+#
+# Avtomobil1 = Avtomobil("damas", "Oq", 1000)
+# print(Avtomobil1)
+
+# # 5 - masala
+#
+# @dataclass
+# class Shahar:
+#     nomi: str
+#     davlat: str
+#     aholi: int
+#
+# Shahar1 = Shahar("Moscow", "Russia", 13_000_000)
+#
+# print(Shahar1)
+
+# # 6 - masala
+#
+# @dataclass
+# class Kompyuter:
+#     protsesor: str
+#     ram: int
+#     xotira: int
+#
+# Kompyuter1 = Kompyuter("i5 10210", 8, 512)
+#
+# print(Kompyuter1)
+
+# # 7 - masala
+#
+# @dataclass
+# class Film:
+#     nomi: str
+#     janr: str
+#     yil: int
+#
+# Film1 = Film("Interstellar", "Ilmiy-fantastika, drama, sarguzasht", 2014)
+#
+# print(Film1)
+
+# # 8 - masala
+#
+# @dataclass
+# class Hisob:
+#     ism: str
+#     hisob_raqam: int
+#     balans: int
+#
+# Hisob1 = Hisob("Eshmat", 555, 1_000_000)
+#
+# print(Hisob1)
+
+# # 9 - masala
+#
+# @dataclass
+# class Oqituvchi:
+#     ism: str
+#     fam: str
+#     tajriba_yili: str
+#
+# Oqituvchi1 = Oqituvchi("Nodir", "Odilov", "???")
+#
+# print(Oqituvchi1)
+
+# # 10 - masala
+#
+# @dataclass
+# class Mahsulot:
+#     nomi: str
+#     narx: int
+#     miqdor: str
+#
+# Mahsulot1 = Mahsulot("cola", 67_000, "shakar va shakar")
+#
+# print(Mahsulot1)
